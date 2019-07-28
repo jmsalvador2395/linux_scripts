3f825786f69bbe6857d7a6a3b83be532ef4236cf
jmsalvador2395 <jmsalvador2395@gmail.com>
2019-07-28 00:37:55 -0700

    started new file name suggestions feature. also added my other scripts

--- py/file_len.py
+++ py/file_len.py
@@ -1,30 +1,60 @@
 import os
 from os import listdir
 from os.path import isfile, join
+import re
 
+#constants
+MAX_FILE_LEN=135
+
+'''
+--------------------------
+may need to use this later
+--------------------------
+
+full path + file -----> join(cwd, f)
+
+'''
+
+#TODO
+#suggest file names for a given string
+def suggest(fname):
+
+	#use temp instead of fname for ease of readability
+	temp=fname
+	suggestions=[]
+
+	#search for comiket prefixes using regex
+	if re.search('\(C..\) ', fname):
+		temp=fname[6:]
+
+		if len(temp)<MAX_FILE_LEN:
+			suggestions.append(temp)
+
+	return suggestions
 
 if __name__ == '__main__':
 	cwd=os.getcwd()
-	#onlyfiles=[]
-	gtr_45=[]
-	lsseq_45=[]
+	gtr_135=[]
+	lsseq_135=[]
+	smallest_gtr_35=''
+
+	#create list of files that exceed 135 characters
 	for f in listdir(cwd):
-		#potential_file=join(cwd, f)
 		potential_file=f
-		if len(f) > 45:
-			gtr_45.append(f)
-		else:
-			lsseq_45.append(f)
-		'''
-		if isfile(potential_file):
-			onlyfiles.append(potential_file)
-		'''
-	print 'greater than 45:'
-	for i in gtr_45:
-		print i
-	
-	print '45 and less:'
-	for i in lsseq_45:
-		print i
+
+		if len(f) > 135:
+			gtr_135.append(f)
+
+			if smallest_gtr_35=='' or len(f)<len(smallest_gtr_35):
+				smallest_gtr_35=f
+
+	#TODO
+	#go through each file to rename
+	if len(gtr_135)>0:
+		print(str(len(gtr_135)) + ' files to rename:\n')
+		for i in gtr_135:
+			suggestions=suggest(i)
+			#print(str(i) + '\n')
 			
-			
+
+		
